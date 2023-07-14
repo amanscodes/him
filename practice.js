d353c1545cca192bcbe50d39b62c851cb36f416f
amanscodes <122535358+amanscodes@users.noreply.github.com>
2023-07-14 00:26:33 -0600

    Delete practice.js

--- practice.js
+++
@@ -1,8 +0,0 @@
-const timeOut=setTimeout(myText,5000);
-function myText(){
-    document.getElementById("text").innerHTML="You are so Beautiful"
-}
-
-function popUp(){
-    alert("Keep blushing like this always")
-}
